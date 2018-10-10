9ff10c343ecce5859a79432e33c3c214428f5478
Bartek Polak <bpolak28@gmail.com>
2018-10-10 12:33:30 +0200

    UPDATE - change to war package, add SpringApplicationBuilder

--- BattleShipsSpringBoot/src/main/java/pl/bpol/BattleShipsSpringBootApplication.java
+++ BattleShipsSpringBoot/src/main/java/pl/bpol/BattleShipsSpringBootApplication.java
@@ -2,9 +2,21 @@ package pl.bpol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@EnableConfigurationProperties
 @SpringBootApplication
-public class BattleShipsSpringBootApplication {
+public class BattleShipsSpringBootApplication extends SpringBootServletInitializer {
+	
+	
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		System.out.println("Inicjalizuję się...");
+		return super.configure(builder);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BattleShipsSpringBootApplication.class, args);
